1fe5503e8b94ed4063cc6acee3e904c0f9aef585
AwesomePotatoCodes <105654727+AwesomePotatoCodes@users.noreply.github.com>
2022-05-16 10:38:37 -0500

    Added test script
    To ensure JS loads.

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+var x = "testing";
+if (x == "testing") {
+  console.log("Javascript has been loaded!")
+}
+
 function githubPage() {
   window.alert("Redirecting...")
   window.open("https://github.com/LolzProject");
